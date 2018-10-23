61f00d2d41572adf42567b041a05e9e5a412af73
batoolalsmael <batool.alsumaeel@gmail.com>
2018-10-22 22:04:27 -0500

    This is the working project that we will be using

--- Waffer/AppDelegate.swift
+++ Waffer/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        
         return true
     }
 
